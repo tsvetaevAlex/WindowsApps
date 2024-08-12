14216bf44198af3d3d7fa04ed5ae578849a28736
@tsvetaev_alex <tsvetaev_alex@git.imbalanced.tech>
2024-08-12 14:06:15 +0300

    add appForm and CusstomElement

---
+++ mdTest/AppForm.cs
@@ -0,0 +1,85 @@
+﻿namespace mdTest
+{
+    partial class appForm : Form
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tabs = new TabControl();
+            TestPage = new TabPage();
+            ReportPage = new TabPage();
+            tabs.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tabs
+            // 
+            tabs.Controls.Add(TestPage);
+            tabs.Controls.Add(ReportPage);
+            tabs.Location = new Point(7, 12);
+            tabs.Name = "tabs";
+            tabs.SelectedIndex = 0;
+            tabs.Size = new Size(815, 437);
+            tabs.TabIndex = 0;
+            // 
+            // TestPage
+            // 
+            TestPage.Location = new Point(4, 24);
+            TestPage.Name = "TestPage";
+            TestPage.Padding = new Padding(3);
+            TestPage.Size = new Size(807, 409);
+            TestPage.TabIndex = 0;
+            TestPage.Text = "Create Test";
+            TestPage.UseVisualStyleBackColor = true;
+            // 
+            // ReportPage
+            // 
+            ReportPage.Location = new Point(4, 24);
+            ReportPage.Name = "ReportPage";
+            ReportPage.Padding = new Padding(3);
+            ReportPage.Size = new Size(807, 409);
+            ReportPage.TabIndex = 1;
+            ReportPage.Text = "ReportPage";
+            ReportPage.UseVisualStyleBackColor = true;
+            // 
+            // appForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(834, 461);
+            Controls.Add(tabs);
+            Name = "appForm";
+            Text = "mdTest";
+            tabs.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TabControl tabs;
+        private TabPage TestPage;
+        private TabPage ReportPage;
+    }
+}
