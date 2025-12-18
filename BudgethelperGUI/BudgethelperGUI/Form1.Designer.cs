9c81f7abfb8dc30e17d501896ed4e5cab8633d61
@tsvetaev_alex <tsvetaev_alex@git.imbalanced.tech>
2025-12-18 14:57:38 +0300

    upload BudgetHekperGUI_status bar

---
+++ BudgethelperGUI/BudgethelperGUI/Form1.Designer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace BudgethelperGUI
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        private RichTextBox rtbFooter;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.rtbFooter = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // rtbFooter
+            // 
+            this.rtbFooter.BackColor = this.BackColor;
+            this.rtbFooter.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.rtbFooter.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.rtbFooter.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbFooter.ForeColor = System.Drawing.Color.White;
+            this.rtbFooter.Location = new System.Drawing.Point(0, 705);
+            this.rtbFooter.Name = "rtbFooter";
+            this.rtbFooter.ReadOnly = true;
+            this.rtbFooter.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.rtbFooter.Size = new System.Drawing.Size(1008, 24);
+            this.rtbFooter.TabIndex = 0;
+            this.rtbFooter.Text = "";
+            // 
+            // MainForm
+            // 
+            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.rtbFooter);
+            this.Name = "MainForm";
+            this.Text = "BudgetHelper v1.0";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+    }
+}
---
+++ BudgethelperGUI/BudgethelperGUI/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BudgethelperGUI
+{
+    public partial class MainForm : Form
+    {
+        private readonly CancellationTokenSource _cts = new CancellationTokenSource();
+        private readonly Stopwatch _stopwatch = Stopwatch.StartNew();
+        private int _heartbeatCounter;
+        private const char heatbeatSpotSymbol = '●';
+        // private string statusBarText = string.Empty; // удалено, не используется
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            _ = RunHeartbeatAsync();
+        }
+
+        private async Task RunHeartbeatAsync()
+        {
+            try
+            {
+                while (!_cts.Token.IsCancellationRequested)
+                {
+                    var t = _stopwatch.Elapsed;
+                    string uptimeStr = $"Uptime: {t:dd\\.hh\\:mm\\:ss}";
+
+                    rtbFooter.BeginInvoke(new Action(() =>
+                    {
+                        rtbFooter.Clear();
+                        rtbFooter.BackColor = Color.Black;
+
+
+                        // Белый текст
+                        rtbFooter.SelectionColor = Color.WhiteSmoke;
+                        rtbFooter.AppendText("Heartbeat  ");
+
+                        // Символ ●
+                        rtbFooter.SelectionColor = (_heartbeatCounter % 2 == 0) ? Color.LimeGreen : Color.Black;
+                        rtbFooter.AppendText("●");
+
+                        // Белый текст с uptime
+                        rtbFooter.SelectionColor = Color.WhiteSmoke;
+                        rtbFooter.AppendText($"  {uptimeStr}");
+                    }));
+
+                    _heartbeatCounter++;
+                    await Task.Delay(500, _cts.Token);
+                }
+            }
+            catch (TaskCanceledException)
+            {
+                // нормальное завершение
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error");
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            _cts.Cancel();
+            base.OnFormClosing(e);
+        }
+    }
+}
