5650929da752fd4bf27972203cc9c14a128f2acc
@tsvetaev_alex <tsvetaev_alex@git.imbalanced.tech>
2025-12-25 20:58:12 +0300

    upload GUI server and Conoslr client

---
+++ Visual.Logger/Visual.Logger.GUI.Server/IVisualLogger.cs
@@ -0,0 +1,15 @@
+﻿using System.ServiceModel;
+
+namespace Visual.Logger.GUI
+{
+    [ServiceContract]
+    public interface IVisualLogger
+    {
+        /// <summary>
+        /// Отправить строковое сообщение в визуальный логгер
+        /// </summary>
+        /// <param name="message">Текст сообщения</param>
+        [OperationContract]
+        void SendMessageToVisualLogger(string message);
+    }
+}
---
+++ Visual.Logger/Visual.Logger.GUI.Server/VisualLoggerForm.Designer.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Forms;
+
+namespace Visual.Logger.GUI
+{
+    public sealed partial class VisualLoggerForm
+    {
+        private RichTextBox loggerTextBox;
+
+        private void InitializeComponent()
+        {
+            this.loggerTextBox = new System.Windows.Forms.RichTextBox();
+
+            this.SuspendLayout();
+
+            this.loggerTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.loggerTextBox.ReadOnly = true;
+
+            this.Controls.Add(this.loggerTextBox);
+
+            this.Text = "Visual Logger";
+            this.ClientSize = new System.Drawing.Size(800, 450);
+
+            this.ResumeLayout(false);
+        }
+    }
+}
